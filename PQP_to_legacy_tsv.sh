e0c2c3faf1489532017aab5736d6211f88198de1
karlssoc <36695929+karlssoc@users.noreply.github.com>
2018-10-31 15:51:53 +0100

    Update PQP_to_legacy_tsv.sh

--- PQP_to_legacy_tsv.sh
+++ PQP_to_legacy_tsv.sh
@@ -2,4 +2,5 @@
 # openswath/openswath:0.1.2::
  TargetedFileConverter -in sample.PQP -out sample.tsv -legacy_traml_id 
  
+# Change column names and file extention 
 sed -e '1s/TransitionId/transition_name/' -e '1s/TransitionGroupId/transition_group_id/' -e '1s/Decoy/decoy/' -e '1s/ProteinId/ProteinName/' -e '1s/NormalizedRetentionTime/Tr_recalibrated/' sample.tsv > sample.csv 
